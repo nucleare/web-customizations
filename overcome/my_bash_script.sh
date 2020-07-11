551a5f0c4c2241f30155543ccc0cdf5cfd33b6fa
chi11estpanda <29671486+nucleare@users.noreply.github.com>
2020-07-11 07:39:47 -0700

    Create my_bash_script.sh
    Only serves as an example for the PHP running a batch script

---
+++ overcome/my_bash_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+svn export --force --no-auth-cache --username myusername --password mypassword http://1.2.3.4/repos/path/to/repo/ $1 2>&1
+find $1 -print0 -type d | xargs -0 -n 1 -0 chown -R -v root:root 2>&1
+find $1'storage' -print0 -type d | xargs -0 -n 1 chown -R -v apache 2>&1
+find $1'shared' -print0 -type d | xargs -0 -n 1 chown -R -v apache 2>&1
+
+## The purpose of the script is to do an export of an svn repo and set user permissions correctly.
+## PHP runs as the apache user.
+## The explanations in the php are to run this script regarding adding the line to sudoers
+## Whereas we're referring to
+##
+## apache ALL=(ALL) NOPASSWD: /bin/bash /var/www/my_bash_script.sh *
