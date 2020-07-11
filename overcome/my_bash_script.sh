b46841634ea4c41d94a4bba32cca8f2491f1102f
chi11estpanda <29671486+nucleare@users.noreply.github.com>
2020-07-11 07:43:28 -0700

    Update my_bash_script.sh

--- overcome/my_bash_script.sh
+++ overcome/my_bash_script.sh
@@ -4,9 +4,11 @@ find $1 -print0 -type d | xargs -0 -n 1 -0 chown -R -v root:root 2>&1
 find $1'storage' -print0 -type d | xargs -0 -n 1 chown -R -v apache 2>&1
 find $1'shared' -print0 -type d | xargs -0 -n 1 chown -R -v apache 2>&1
 
-## The purpose of the script is to do an export of an svn repo and set user permissions correctly.
-## PHP runs as the apache user.
-## The explanations in the php are to run this script regarding adding the line to sudoers
-## Whereas we're referring to
-##
-## apache ALL=(ALL) NOPASSWD: /bin/bash /var/www/my_bash_script.sh *
+/**
+* The purpose of the script is to do an export of an svn repo and set user permissions correctly.
+* PHP runs as the apache user.
+* The explanations in the php are to run this script regarding adding the line to sudoers
+* Whereas we're referring to
+*
+* apache ALL=(ALL) NOPASSWD: /bin/bash /var/www/my_bash_script.sh *
+*/
